16c4fd0df8559d7f7f9c758bae84c52894ecc38b
Christopher Stott <christopherstott@gmail.com>
2015-03-13 16:21:36 -0700

    HelloWorldCocoapods

---
+++ iOS/HelloWorldCocoapods/HelloWorldCocoapods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorldCocoapods
+//
+//  Created by Chris on 2015-03-13.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
