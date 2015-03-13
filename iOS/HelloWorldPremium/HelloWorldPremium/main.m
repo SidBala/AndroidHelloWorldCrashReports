266e56230270846574628591850dc41d50e1324d
Christopher Stott <christopherstott@gmail.com>
2015-03-13 16:58:28 -0700

    HelloWorldPremium

---
+++ iOS/HelloWorldPremium/HelloWorldPremium/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorldPremium
+//
+//  Created by Chris on 2015-03-13.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
