2d7740cfb1decdd5a873527815707b1e73800fe1
Sid Bala <Sid.Bala@Outlook.com>
2015-12-15 14:49:03 -0800

    Adding an application class

---
+++ app/src/main/java/buddybuild/com/helloworldandroid/MyApplication.java
@@ -0,0 +1,10 @@
+package buddybuild.com.helloworldandroid;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
